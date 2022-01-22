24521ecab1210220ff3f649ceeb2857f03828378
GratasXD <gratasbov@gmail.com>
2022-01-22 16:08:51 +0200

    added: leetcode dir and strstr problem

---
+++ LeetCode/Implement strStr()/Solution.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+ var strStr = function(haystack, needle) {
+    let text = "Hello world, welcome to the universe.";
+    let result = text.indexOf("welcome");
+};
